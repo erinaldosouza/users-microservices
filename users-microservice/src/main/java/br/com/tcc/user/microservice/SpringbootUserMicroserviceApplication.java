6429c727aa4ea02785af77ef4dd78f516aaf2380
t_francisco <t_francisco@MATRIZ-300681.hapvida.com.br>
2019-04-03 17:09:34 -0300

    hystrix config - started

--- users-microservice/src/main/java/br/com/tcc/user/microservice/SpringbootUserMicroserviceApplication.java
+++ users-microservice/src/main/java/br/com/tcc/user/microservice/SpringbootUserMicroserviceApplication.java
@@ -2,12 +2,14 @@ package br.com.tcc.user.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrix
 @EnableEurekaClient
-@EnableCircuitBreaker
 @SpringBootApplication
+@EnableHystrixDashboard
 public class SpringbootUserMicroserviceApplication {
 
 	public static void main(String[] args) {
--- users-microservice/src/main/java/br/com/tcc/user/microservice/business/impl/UserServiceImpl.java
+++ users-microservice/src/main/java/br/com/tcc/user/microservice/business/impl/UserServiceImpl.java
@@ -95,7 +95,7 @@ public class UserServiceImpl implements UserService {
 			errorCode = Integer.valueOf(exception.getMessage().replaceAll("\\D", ""));
 		
 		} catch (Exception e) {
-			e.printStackTrace();
+			// nobody helpfull to print herer e.printStackTrace();
 		}
 		
 		return ResponseEntity.status(errorCode).body(wrapper);
