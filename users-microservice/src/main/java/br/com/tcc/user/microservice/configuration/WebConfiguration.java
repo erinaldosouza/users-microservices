79ffa5109e1cfe98f18d85eace80a2dc59adb763
t_francisco <t_francisco@MATRIZ-300681.mshome.net>
2019-06-24 10:51:15 -0300

    defined 5 seconds to connect timeout

--- users-microservice/src/main/java/br/com/tcc/user/microservice/configuration/WebConfiguration.java
+++ users-microservice/src/main/java/br/com/tcc/user/microservice/configuration/WebConfiguration.java
@@ -2,14 +2,27 @@ package br.com.tcc.user.microservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class WebConfiguration {
 	
 	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+	protected RestTemplate getRestTemplate() {
+		return new RestTemplate(getClientHttpRequestFactory());
+	}
+
+	private ClientHttpRequestFactory getClientHttpRequestFactory() {
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		
+		// Connect timeout
+		clientHttpRequestFactory.setConnectTimeout(5000);
+		
+		// Read timeout
+		// clientHttpRequestFactory.setReadTimeout(0);
+		return clientHttpRequestFactory;
 	}
 
 }
